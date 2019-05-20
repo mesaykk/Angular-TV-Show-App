c5638543dfbd47348bd7253870139f2c574db10b
Hannah0124 <hannahjoo24@gmail.com>
2019-05-19 18:40:53 -0700

    Show Result Component

--- tv-show-app/src/app/app.module.ts
+++ tv-show-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShowResultComponent } from './show-result/show-result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShowResultComponent
   ],
   imports: [
     BrowserModule,
